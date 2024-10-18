f769f1dbee92196a8ce2ac0d15c5f17b26192762
Christian Z <90133763+macelivity@users.noreply.github.com>
2024-10-18 11:31:41 +0200

    Removed isync.py

--- isync.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-HOST_USER = "fbgb"
-HOST_ADDRESS = "fbg-bremen.de"
-HOST_DIRECTORY = "/photobooth/website/images"
-
-def sync_with_server():
-    subprocess.run(, shell = True, executable="/bin/bash")
